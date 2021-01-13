f183d09d8f4c248a400cd0dc683be272806ea9b0
HamnYce <hamadboa@gmail.com>
2021-01-13 17:24:22 +0300

    returned create.py

---
+++ create.py
@@ -0,0 +1,21 @@
+import os
+import requests
+slug = input("What is the url/slug of the Kata?")
+if ".com" in slug:
+    slug = slug.split("/")[-1]
+url = f"https://www.codewars.com/api/v1/code-challenges/{slug}"
+
+resp = requests.get(url=url)
+data = resp.json()
+name = data['name']
+desc = data['description']
+kyu = data["rank"]['name']
+print(kyu)
+dname = f"{name} - {kyu}"
+os.mkdir(dname)
+os.chdir(dname)
+md = open(f"{name}.md","w")
+md.write(desc)
+md.close()
+py = open(f"solution.py","w")
+py.close()
