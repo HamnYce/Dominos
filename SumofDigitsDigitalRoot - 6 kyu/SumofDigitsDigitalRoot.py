471f0c2c4a21ff33aeaa76574806d84a3336c67d
HamnYce <Hamadboa@gmail.com>
2021-01-16 17:33:30 +0300

    random fix

--- SumofDigitsDigitalRoot - 6 kyu/SumofDigitsDigitalRoot.py
+++ SumofDigitsDigitalRoot - 6 kyu/SumofDigitsDigitalRoot.py
@@ -1,20 +1,20 @@
-# def digital_root(n):
-#     x = 0
-#     while True:
-#         for i in str(n):
-#             x += int(i)
-#         n = x
-        
-#         if len(str(x)) == 1:
-#             break
-#         x = 0
-#     return x 
-    
-# print(digital_root(169))
-
-
-
 def digital_root(n):
-    return n if n < 10 else digital_root(sum(map(int,str(n))))
+    x = 0
+    while True:
+        for i in str(n):
+            x += int(i)
+        n = x
+        
+        if len(str(x)) == 1:
+            break
+        x = 0
+    return x 
+    
+print(digital_root(169))
 
-print(map(int,str(169)))
+
+#OP one liner
+# def digital_root(n):
+#     return n if n < 10 else digital_root(sum(map(int,str(n))))
+
+# print(map(int,str(169)))
